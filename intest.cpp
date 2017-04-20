b29f2544b56399a7e7e2148ef60ed9b4aa265b85
IN <nompelis@nobelware.com>
2017-04-19 20:59:15 -0500

    Created a class to perform "face-matching" for (nearly) planar elements. Added the constructor and deconstructor of the object and created an API function that is visible from C codes; appropriate modifications to the header (.h) file was made.

--- intest.cpp
+++ intest.cpp
@@ -6,12 +6,58 @@
 
 #ifdef __cplusplus
 extern "C" {
+
+
+//
+// Constructor of face-matcher object
+//
+incg_FaceMatcher::incg_FaceMatcher( MPI_Comm *comp )
+{
+#ifdef _DEBUG_
+   printf("incg_FaceMatcher object instantiated\n");
 #endif
 
+   MPI_Comm_dup( *comp, &comm );
+   MPI_Comm_size( comm, &nproc );
+   MPI_Comm_rank( comm, &irank );
+
+
+}
+
+//
+// Deconstructor of face-matcher object
+//
+incg_FaceMatcher::~incg_FaceMatcher( )
+{
+
+
+   MPI_Comm_free( &comm );
+}
 
 
 
-#ifdef __cplusplus
+//
+// API function to invoke the functionality from the C side
+//
+int incg_PerformFacematch( MPI_Comm *comm, int icnt1, double *x1,
+                                           int icnt2, double *x2 )
+{
+   int nproc,irank;
+   MPI_Comm_size( *comm, &nproc );
+   MPI_Comm_rank( *comm, &irank );
+   printf("The function was invoked by MPI process %d \n", irank);
+
+
+   incg_FaceMatcher fm( comm );
+
+
+
+
+
+   return(0);
+}
+
+
 }
 #endif
 
--- intest.h
+++ intest.h
@@ -6,25 +6,41 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+//
+// API function prototype visible to the C side to invoke the functionality
+//
+int incg_PerformFacematch( MPI_Comm *comm, int icnt1, double *x1,
+                                           int icnt2, double *x2 );
 
-class myClass {
+#ifdef __cplusplus
+}
+#endif
 
+
+#ifdef __cplusplus
+extern "C" {
+
+class incg_FaceMatcher {
  public:
+   incg_FaceMatcher( MPI_Comm *comp );
+   ~incg_FaceMatcher();
 
  protected:
 
- public:
-
+ private:
+   MPI_Comm comm;
+   int irank;
+   int nproc;
 
 };
 
 
 
-#ifdef __cplusplus
 }
 #endif
 
